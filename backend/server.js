f38e578ce9000c26a1864d7b1d7d10b07561a899
PhoeniX <rpx03775@gmail.com>
2025-08-31 16:58:53 +0530

    updated cors in backend

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,8 @@ try{
 }
 
 app.use(cors({
-    // origin: 'http://localhost:5173',
-    origin: 'https://r-smart.onrender.com',
-    credentials: false,
+    origin: process.env.CLIENT_URL,
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
