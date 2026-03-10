2d55bf765e083dc3984340fbf2094290682cd3ba
Lakshya Kumar <rpx03775@gmail.com>
2026-03-10 16:56:18 +0530

    update node versions in docker file and backend url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,3 +1,4 @@
 // Backend API Base URL
 // export const BACKEND_BASE_URL = "http://localhost:5000";
-export const BACKEND_BASE_URL = import.meta.env.VITE_SERVER_URL;
+export const BACKEND_BASE_URL = 'https://r-smart-backend.onrender.com'; 
+// export const BACKEND_BASE_URL = import.meta.env.VITE_SERVER_URL;
